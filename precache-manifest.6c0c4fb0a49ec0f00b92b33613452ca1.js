cec5bf28503125ba8280bbdf23d67baac620d35b
Phoenix <drake.mcgarrah@gmail.com>
2020-08-16 11:16:54 -0500

    Updates

---
+++ precache-manifest.6c0c4fb0a49ec0f00b92b33613452ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12eb4c4fb75a44e5d8925fdb8828b38d",
+    "url": "/blog-author/index.html"
+  },
+  {
+    "revision": "8fc1fac9e23fc9512b6a",
+    "url": "/blog-author/static/css/main.c5fd0cda.chunk.css"
+  },
+  {
+    "revision": "5a7653c8a852167ebc40",
+    "url": "/blog-author/static/js/2.f3aed338.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/blog-author/static/js/2.f3aed338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc1fac9e23fc9512b6a",
+    "url": "/blog-author/static/js/main.40e55c4a.chunk.js"
+  },
+  {
+    "revision": "a2caea342d044a9aed26",
+    "url": "/blog-author/static/js/runtime-main.1372ef83.js"
+  }
+]);
