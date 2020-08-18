d47a6381bbfc8e16918ab6a70bf1e5798226aecc
Phoenix <drake.mcgarrah@gmail.com>
2020-08-18 10:32:30 -0500

    Fixed import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WritePost from "./pages/WritePost";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
